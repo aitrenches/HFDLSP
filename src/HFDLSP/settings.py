50a6e3802ee9a1cd5771d23546c42d7dbd23ce97
Quyum Kehinde <quyumkehinde@gmail.com>
2023-12-27 13:02:45 +0100

    chore: cleanup view

--- src/HFDLSP/settings.py
+++ src/HFDLSP/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
 
 # Neo4js Settings
 NEOMODEL_NEO4J_BOLT_URL = os.getenv(
--- src/data_transformer/views.py
+++ src/data_transformer/views.py
@@ -2,10 +2,6 @@ from django.views.decorators.http import require_GET
 from django.http import JsonResponse, HttpResponseBadRequest
 from data_retriever.models import TimeQADataset, HotpotQADataset, TreeOfKnowledgeDataset
 from HFDLSP.settings import DATASET_IDS
-from rest_framework_swagger.views import get_swagger_view
-
-
-schema_view = get_swagger_view(title="HFDLSP API")
 
 
 @require_GET
@@ -13,29 +9,35 @@ def answer_view(request):
     user_query = request.GET.get("query")
     dataset_id = request.GET.get("dataset")
 
-    if not user_query:
-        return HttpResponseBadRequest({"error": "No query provided."})
-    if not dataset_id:
-        return HttpResponseBadRequest({"error": "No dataset provided."})
-    if not DATASET_IDS.get(dataset_id):
-        return HttpResponseBadRequest({"error": "The dataset ID is invalid."})
+    try:
+        if not user_query:
+            return HttpResponseBadRequest({"error": "No query provided."})
+        if not dataset_id:
+            return HttpResponseBadRequest({"error": "No dataset provided."})
+        if not DATASET_IDS.get(dataset_id):
+            return HttpResponseBadRequest({"error": "The dataset ID is invalid."})
 
-    if dataset_id == "tree_of_knowledge":
-        dataset = TreeOfKnowledgeDataset.objects.get(name=dataset_id).questions.filter(
-            text=user_query
-        )
-        return JsonResponse({"result": dataset[0].answer})
+        result = None
+        if dataset_id == "tree_of_knowledge":
+            dataset = TreeOfKnowledgeDataset.nodes.first_or_none(question=user_query)
+            result = dataset.answer if dataset else None
 
-    if dataset_id == "hotpot_qa":
-        dataset = HotpotQADataset.objects.get(name=dataset_id).questions.filter(
-            text=user_query
-        )
-        return JsonResponse({"result": dataset[0].answers.all()[0].text})
+        if dataset_id == "hotpot_qa":
+            dataset = HotpotQADataset.nodes.first_or_none(question=user_query)
+            result = dataset.answer if dataset else None
 
-    if dataset_id == "time_qa":
-        dataset = TimeQADataset.objects.get(name=dataset_id).questions.filter(
-            text=user_query
-        )
-        return JsonResponse({"result": dataset[0].answers.all()[0].text})
+        if dataset_id == "time_qa":
+            dataset = (
+                TimeQADataset.nodes.first_or_none(question=user_query)
+                .fetch_relations("answers")
+                .answers.first_or_none()
+            )
+            result = dataset.value if dataset else None
 
-    return JsonResponse({"result": "No answer found."})
+        if result:
+            return JsonResponse({"result": result})
+
+        return JsonResponse({"result": "No answer found."})
+    except Exception as e:
+        print(e)
+        return JsonResponse({"error": "An unknown error occurred."})
