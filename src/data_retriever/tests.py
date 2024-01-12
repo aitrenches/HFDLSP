d1b8e4365d199cfeb5edb49f6b6524123584b05b
Quyum Kehinde <quyumkehinde@gmail.com>
2024-01-12 17:58:57 +0100

    feat: more unit tests

--- src/data_retriever/tests.py
+++ src/data_retriever/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from unittest import mock
-from .service import fetch_huggingface_dataset
+from .service import fetch_huggingface_dataset, insert_dataset_into_neo4j
 
 
 class FetchHuggingFaceDatasetTests(TestCase):
@@ -15,3 +15,17 @@ class FetchHuggingFaceDatasetTests(TestCase):
         mock_load_dataset.side_effect = Exception("Dataset not found")
         dataset = fetch_huggingface_dataset("invalid_dataset")
         self.assertIsNone(dataset)
+
+
+class InsertDatasetIntoNeo4jTests(TestCase):
+    @mock.patch("data_retriever.models.TreeOfKnowledgeDataset.create")
+    def test_insert_success(self, mock_model_create):
+        data = {
+            "question": "What is Biology",
+            "answer": "Bilogy is a branch of science",
+        }
+        mock_model_create.return_value = None
+        insert_dataset_into_neo4j(
+            "tree_of_knowledge",
+            data,
+        )
