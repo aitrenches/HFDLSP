bab36ae8f5be42e9e13fa64d3f28c0b5540266b2
Quyum Kehinde <quyumkehinde@gmail.com>
2024-01-12 17:42:25 +0100

    feat: added unit tests

--- src/data_retriever/tests.py
+++ src/data_retriever/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from unittest import mock
+from .service import fetch_huggingface_dataset
 
-# Create your tests here.
+
+class FetchHuggingFaceDatasetTests(TestCase):
+    @mock.patch("datasets.load_dataset")
+    def test_valid_dataset(self, mock_load_dataset):
+        mock_load_dataset.return_value = "test"
+        dataset = fetch_huggingface_dataset("time_qa")
+        self.assertEqual(dataset, "test")
+
+    @mock.patch("datasets.load_dataset")
+    def test_invalid_dataset(self, mock_load_dataset):
+        mock_load_dataset.side_effect = Exception("Dataset not found")
+        dataset = fetch_huggingface_dataset("invalid_dataset")
+        self.assertIsNone(dataset)
